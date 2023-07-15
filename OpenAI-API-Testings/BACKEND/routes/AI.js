8ade7379f048da8e739dd80aa22c60dc38513d95
djd <dananjayahbi@gmail.com>
2023-07-16 02:17:55 +0530

    some openAI API testings

--- OpenAI-API-Testings/BACKEND/routes/AI.js
+++ OpenAI-API-Testings/BACKEND/routes/AI.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 
 const {
-    processPrompt,
+  processPrompt,
 } = require("../controllers/AIresponseController");
 
-// GET AI RESPONSE
+// POST request to process the initial prompt
 router.post("/processPrompt", processPrompt);
 
 module.exports = router;
